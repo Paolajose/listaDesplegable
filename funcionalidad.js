1ef00c51dc0c9e686a863420ae4e9cf5b4d1763e
paolajose <paoj398@gmail.com>
2021-01-21 17:05:18 -0400

    terminado

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,66 +1,62 @@
  
 let seleccion = document.getElementById("selector")
+let lista= document.getElementById("lista")
+
 
 elegir();
 
 
 function elegir(){
-    
- 
 
-    fetch("https://restcountries.eu/rest/v2/all?fields=name;capital;currencies",{
-                })
+  fetch("https://restcountries.eu/rest/v2/all?fields=name;capital;currencies",{
+  })
+        
+    .then( function(response){
+        if   (response.ok){
+          return response.json()
+        }else {
+         throw "error en la llamada ajax"
+        }
+    })
 
-               
-             .then( function(response){
-                   if   (response.ok){
-                 return response.json()
-                   }else {
-                     throw "error en la llamada ajax"
-                    }
-               })
+    .then (function(texto) {
+      console.log(texto)
+                  
+        for (let i = 0; i <=texto.length - 1; i ++){
+          console.log(texto[i].name)
+          let opcion = document.createElement("option");
+          let valor= texto;
+          opcion.text= valor[i].name;
+          opcion.value= valor[i].capital;
+          seleccion.appendChild(opcion);
+          text = valor[i].name;
+          value=valor[i].capital;    
+        };
 
-             .then (function(texto) {
-                 console.log(texto)
-                 
-                    for (let i = 0; i <=texto.length - 1; i ++){
-                     console.log(texto[i].name)
-                 
+        function opcionCambiada(){
+            
+            mostrar();
+           
+        };
 
-                      let opcion = document.createElement("option");
-                      let valor= texto;
-                      opcion.text= valor[i].name;
-                      opcion.value= valor[i].capital;
-    
-                       seleccion.appendChild(opcion);
+        seleccion.addEventListener("change" , opcionCambiada );
+        
+        function mostrar(){
+            let indice = seleccion.selectedIndex;
+            let opcionSeleccionada= seleccion.options[indice];
+            lista.innerHTML= ("Pais :" + " " + opcionSeleccionada.text  + " " + "Capital : " + opcionSeleccionada.value);
+            
+           
 
-                       name = valor[i].name
-                       capital=valor[i].capital
-                       
-                      
-                         
-                    
+        }
+          
+                
+    });
 
-                     mostrar()
-
-                     function mostrar(){
-                      let indice = seleccion.selectedIndex
-                      if (indice === 1) return;
-
-                      
-
-                     }
-
-                    
-
-                  };
-
-                   alert("seleccionaste " + name + " " + capital)
-
-
-                    });
+};
 
              
- };
+ 
+
     
 
